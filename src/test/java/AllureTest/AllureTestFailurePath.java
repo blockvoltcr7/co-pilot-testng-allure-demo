0a69f734cf79846eb476add85923d19b9cc4b379
Sami Sabir-Idrissi <sami_sabir-idrissi@epam.com>
2024-05-07 21:45:36 -0400

    adding maven sure fire clean plugin in order to reset the allure-results folder

--- src/test/java/AllureTest/AllureTestFailurePath.java
+++ src/test/java/AllureTest/AllureTestFailurePath.java
@@ -1,5 +1,6 @@
 package AllureTest;
 
+import io.qameta.allure.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,8 +8,11 @@ public class AllureTestFailurePath {
 
 
     @Test
+    @Description("Lambda step test1")
     public void testMethod() {
         System.out.println("Hello, world! im going to fail on purpose");
         Assert.assertEquals(1, 2, "This test is expected to fail");
     }
+
+
 }
--- src/test/java/AllureTest/AllureTestHappyPath.java
+++ src/test/java/AllureTest/AllureTestHappyPath.java
@@ -1,5 +1,6 @@
 package AllureTest;
 
+import io.qameta.allure.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,6 +8,7 @@ public class AllureTestHappyPath {
 
 
     @Test
+    @Description("Lambda step test2")
     public void testMethodHappyPath() {
         System.out.println("Hello, world! im going to Pass");
         Assert.assertEquals(1, 1, "This test is expected to fail");
